e2a45d831f2afce2b8147d57c18aabfc2c9c7fb7
V3Ronica1 <veronicagarciaperez888@gmail.com>
2023-05-17 17:36:09 -0600

    Login

--- registro.php
+++ registro.php
@@ -1,3 +1,20 @@
+<?php
+
+require_once("clases/conexion.php");
+$obj = new conectar();
+$conexion = $obj->conexion();
+
+$sql = "SELECT * from usuarios where email='admin'";
+$result = mysqli_query($conexion, $sql);
+$validar = 0;
+if(mysqli_num_rows($result) > 0){
+    header("location:index.php");
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +24,8 @@
     <title>Registro</title>
     <link rel="stylesheet" type="text/css" href="librerias/bootstrap1/css/bootstrap.css">
     <script src="librerias/jquery-3.2.1.min.js"></script>
+    <script src="js/funciones.js"></script>
+
 </head>
 <body style="background-color: gray;">
     <div class="container" style="margin-top: 75px;">
@@ -16,7 +35,7 @@
                 <div class="panel panel-danger">
                     <div class="panel panel-heading">Registrar Administrador</div>
                     <div class="panel panel-body">
-                        <form id="frnRegistro">
+                        <form id="frmRegistro">
                             <label>Nombre</label>
                             <input type="text" class="form-control input-sm" id="nombre" name="nombre">
 
@@ -29,7 +48,7 @@
                             <label>Contraseña</label>
                             <input type="text" class="form-control input-sm" id="password" name="password">
                             <p></p>
-                            <span class="btn btn-primary">Registrar</span>
+                            <span class="btn btn-primary" id="registro" >Registrar</span>
                             <a href="index.php" class="btn btn-default">Regresar Login</a>
                         </form>
                     </div>
@@ -40,3 +59,37 @@
     </div>
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#registro').click(function(){
+
+
+           vacios=validarFormVacio('frmRegistro');
+           
+           if(vacios > 0){
+          alert("Debes llenar todos los campos!!");
+          return false;
+        }
+
+            datos=$('#frmRegistro').serialize();
+            $.ajax({
+                type:"POST" ,
+                data:datos,
+                url:"procesos/regLogin/registrarUsuarios.php",
+                success:function(r){
+                    alert(r);
+                    if(r==1){
+                        alert("Agregado con exito");
+                    }else{
+                        alert("Fallo al agregar");
+                    }
+
+                }
+
+            });
+
+        });
+    });
+
+    </script>
