2af00d078d7414c498c4af5f909fbe16fa9aaa2f
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-05-20 16:59:54 -0600

    Maquetacion de modulo artículos

--- vistas/articulos.php
+++ vistas/articulos.php
@@ -1,26 +1,84 @@
 <?php
-
-    session_start();
-    if(isset($_SESSION['usuario'])){
-   
+session_start();
+if (isset($_SESSION['usuario'])) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
+  <!DOCTYPE html>
+  <html lang="es">
+
+  <head>
     <title>Articulos</title>
     <?php require_once "menu.php"; ?>
-</head>
-<body>
-    
-</body>
-</html>
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>Articulos</h1>
+      <div class="row">
+        <div class="col-sm-4">
+          <form id="frmArticulos" enctype="multipart/form-data">
+            <label>Categoria</label>
+            <select class="form-control input-sm" id="categoriaSelect" name="categoriaSelect">
+              <option value="A">Selecciona Categoria</option>
+            </select>
+            <label>Nombre</label>
+            <input type="text" class="form-control input-sm" id="nombre" name="nombre">
+            <label>Descripcion</label>
+            <input type="text" class="form-control input-sm" id="descripcion" name="descripcion">
+            <label>Cantidad</label>
+            <input type="text" class="form-control input-sm" id="cantidad" name="cantidad">
+            <label>Precio</label>
+            <input type="text" class="form-control input-sm" id="precio" name="precio">
+            <label>Imagen</label>
+            <input type="file" id="imagen" name="imagen">
+            <p></p>
+            <span id="btnAgregaArticulo" class="btn btn-primary">Agregar</span>
+          </form>
+        </div>
+        <div class="col-sm-8">
+          <div id="tablaArticulosLoad"></div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+  </html>
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#tablaArticulosLoad').load("articulos/tablaArticulos.php");
+
+      $('#btnAgregaArticulo').click(function() {
+
+        vacios=validarFormVacio('frmArticulos');
+
+				if(vacios > 0){
+					alertify.alert("Debes llenar todos los campos!!");
+					return false;
+				}
+
+        datos = $('#frmArticulos').serialize();
+        $.ajax({
+          type: "POST",
+          data: datos,
+          url: "../procesos/articulos/agregaArticulos.php",
+          success: function(r) {
+            if(r==1){
+              alertify.success("Agregado con exito :)");
+            }else{
+              alertify.error("Error al agregar articulo :(");
+            }
+          }
+        });
+      });
+    });
+  </script>
 
 <?php
-  }else{
-    
-header("location:../index.php");
-  }
+} else {
+
+  header("location:../index.php");
+}
 
 ?>
