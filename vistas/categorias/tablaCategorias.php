9eb0f2818f3cda3345e4139c96017a242a298294
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-05-21 10:14:32 -0600

    Mostrar datos de categorias

--- vistas/categorias/tablaCategorias.php
+++ vistas/categorias/tablaCategorias.php
@@ -1,4 +1,14 @@
+<?php
 
+require_once "../../clases/Conexion.php";
+$c = new conectar();
+$conexion = $c->conexion();
+
+$sql = "SELECT id_categoria,nombreCategoria FROM categorias";
+
+$result = mysqli_query($conexion,$sql);
+
+?>
 
 <table class="table table-hover table-condensed table-bordered" style="text-align: center;">
 	<caption><label>Categorias :D</label></caption>
@@ -8,17 +18,22 @@
 		<td>Eliminar</td>
 	</tr>
 
+	<?php
+	while ($ver=mysqli_fetch_row($result)):
+	?>
+
 	<tr>
-		<td></td>
+		<td><?php echo $ver[1] ?></td>
 		<td>
 			<span class="btn btn-warning btn-xs">
 				<span class="glyphicon glyphicon-pencil"></span>
 			</span>
 		</td>
-        <td>
+		<td>
 			<span class="btn btn-danger btn-xs">
 				<span class="glyphicon glyphicon-remove"></span>
 			</span>
 		</td>
 	</tr>
+	<?php endwhile; ?>
 </table>
