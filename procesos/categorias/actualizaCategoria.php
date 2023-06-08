c52e9fae0b2dce1deb0f507e6473f47cd400177f
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-06-07 18:10:43 -0600

    Corrigiendo Error en tabla Categorias

--- procesos/categorias/actualizaCategoria.php
+++ procesos/categorias/actualizaCategoria.php
@@ -1,15 +1,16 @@
-<?php
-require_once "../../clases/Conexion.php";
-require_once "../../clases/Categorias.php";
+<?php 
+	require_once "../../clases/Conexion.php";
+	require_once "../../clases/Categorias.php";
 
+	
 
+	$datos=array(
+		$_POST['idcategoria'],
+		$_POST['categoriaU']
+			);
 
-$datos = array(
-    $_POST['idcategoria'],
-    $_POST['categoriaU']
-);
+	$obj= new categorias();
 
-$obj = new categorias();
+	echo $obj->actualizaCategoria($datos);
 
-echo $obj->actualizaCategoria($datos);
-?>
+ ?>
--- vistas/categorias.php
+++ vistas/categorias.php
@@ -1,18 +1,16 @@
-<?php
+<?php 
 session_start();
-if (isset($_SESSION['usuario'])) {
+if(isset($_SESSION['usuario'])){
 
-?>
+	?>
 
 
 	<!DOCTYPE html>
 	<html>
-
 	<head>
 		<title>categorias</title>
 		<?php require_once "menu.php"; ?>
 	</head>
-
 	<body>
 
 		<div class="container">
@@ -34,27 +32,25 @@ if (isset($_SESSION['usuario'])) {
 
 		<!-- Button trigger modal -->
 
-
 		<!-- Modal -->
 		<div class="modal fade" id="actualizaCategoria" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 			<div class="modal-dialog modal-sm" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">Actualiza Categorias</h4>
+						<h4 class="modal-title" id="myModalLabel">Actualiza categorias</h4>
 					</div>
 					<div class="modal-body">
 						<form id="frmCategoriaU">
-							<input type="text" name="idcategoria" id="idcategoria" hidden="">
-							<label for="">Categoria</label>
-							<input type="text" name="categoriaU" id="categoriaU" class="form-control input-sm">
-
+							<input type="text" hidden="" id="idcategoria" name="idcategoria">
+							<label>Categoria</label>
+							<input type="text" id="categoriaU" name="categoriaU" class="form-control input-sm">
 						</form>
 
 
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn btn-warning" data-dismiss="modal" id="btnActualizaCategoria">Guardar</button>
+						<button type="button" id="btnActualizaCategoria" class="btn btn-warning" data-dismiss="modal">Guardar</button>
 
 					</div>
 				</div>
@@ -62,96 +58,92 @@ if (isset($_SESSION['usuario'])) {
 		</div>
 
 	</body>
-
 	</html>
 	<script type="text/javascript">
-		$(document).ready(function() {
+		$(document).ready(function(){
 
 			$('#tablaCategoriaLoad').load("categorias/tablaCategorias.php");
 
-			$('#btnAgregaCategoria').click(function() {
+			$('#btnAgregaCategoria').click(function(){
 
-				vacios = validarFormVacio('frmCategorias');
+				vacios=validarFormVacio('frmCategorias');
 
-				if (vacios > 0) {
+				if(vacios > 0){
 					alertify.alert("Debes llenar todos los campos!!");
 					return false;
 				}
 
-				datos = $('#frmCategorias').serialize();
+				datos=$('#frmCategorias').serialize();
 				$.ajax({
-					type: "POST",
-					data: datos,
-					url: "../procesos/categorias/agregaCategoria.php",
-					success: function(r) {
-						if (r == 1) {
-							//esta linea nos permite limpiar el formulario al insertar un registro
-							$('#frmCategorias')[0].reset();
-							$('#tablaCategoriaLoad').load("categorias/tablaCategorias.php");
-							alertify.success("Categoria agregada con exito :D");
-						} else {
-							alertify.error("No se pudo agregar categoria");
-						}
-					}
-				});
+					type:"POST",
+					data:datos,
+					url:"../procesos/categorias/agregaCategoria.php",
+					success:function(r){
+						if(r==1){
+					//esta linea nos permite limpiar el formulario al insetar un registro
+					$('#frmCategorias')[0].reset();
+
+					$('#tablaCategoriaLoad').load("categorias/tablaCategorias.php");
+					alertify.success("Categoria agregada con exito :D");
+				}else{
+					alertify.error("No se pudo agregar categoria");
+				}
+			}
+		});
 			});
 		});
 	</script>
 
 	<script type="text/javascript">
-		$(document).ready(function() {
-			//script para evento click y ajax 
-			$('#btnActualizaCategoria').click(function() {
+		$(document).ready(function(){
+			$('#btnActualizaCategoria').click(function(){
 
-				datos = $('#frmCategoriaU').serialize();
+				datos=$('#frmCategoriaU').serialize();
 				$.ajax({
-					type: "POST",
-					data: datos,
-					url: "../procesos/categorias/actualizaCategoria.php",
-					success: function(r) {
-						if (r == 1) {
+					type:"POST",
+					data:datos,
+					url:"../procesos/categorias/actualizaCategoria.php",
+					success:function(r){
+						if(r==1){
 							$('#tablaCategoriaLoad').load("categorias/tablaCategorias.php");
 							alertify.success("Actualizado con exito :)");
-						} else {
-							alertify.error("No se puede actualizar :(");
+						}else{
+							alertify.error("no se pudo actaulizar :(");
 						}
-
 					}
 				});
 			});
 		});
 	</script>
 
-
 	<script type="text/javascript">
-		function agregaDato(idCategoria, categoria) {
+		function agregaDato(idCategoria,categoria){
 			$('#idcategoria').val(idCategoria);
 			$('#categoriaU').val(categoria);
 		}
 
-		function eliminaCategoria(idcategoria) {
-			alertify.confirm('¿Desea eliminar esta categoria?', function() {
+		function eliminaCategoria(idcategoria){
+			alertify.confirm('¿Desea eliminar esta categoria?', function(){ 
 				$.ajax({
-					type: "POST",
-					data: "idcategoria=" + idcategoria,
-					url: "../procesos/categorias/eliminarCategoria.php",
-					success: function(r) {
-						if (r == 1) {
+					type:"POST",
+					data:"idcategoria=" + idcategoria,
+					url:"../procesos/categorias/eliminarCategoria.php",
+					success:function(r){
+						if(r==1){
 							$('#tablaCategoriaLoad').load("categorias/tablaCategorias.php");
 							alertify.success("Eliminado con exito!!");
-						} else {
+						}else{
 							alertify.error("No se pudo eliminar :(");
 						}
 					}
 				});
-			}, function() {
+			}, function(){ 
 				alertify.error('Cancelo !')
 			});
 		}
 	</script>
-
-<?php
-} else {
+	<?php 
+}else{
 	header("location:../index.php");
 }
 ?>
